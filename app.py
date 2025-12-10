0cd83a2b349b5e2d22e7e0722933191df3dd983d
thohongxjnh <thohongxjnh@gmail.com>
2025-12-10 10:50:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nguyễn Thị Xuân - Trường TH Cầu Ham - ĐT: 0913849892 </p>
 </div>
 """, unsafe_allow_html=True)
 
